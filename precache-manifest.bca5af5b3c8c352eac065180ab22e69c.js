10e8bea43c0240a9ca829e193a01e7d710ac34b7
Gurleen <sarusethi@rocketmail.com>
2020-02-03 03:03:16 +0000

    Update

---
+++ precache-manifest.bca5af5b3c8c352eac065180ab22e69c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0687ec99ab8f02224b0e1697b3d356f4",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "01e8272593e1e781ab39",
+    "url": "/would-you-rather/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "674254b2ad071f88e141",
+    "url": "/would-you-rather/static/js/2.9d7a22aa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/would-you-rather/static/js/2.9d7a22aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01e8272593e1e781ab39",
+    "url": "/would-you-rather/static/js/main.00f3b885.chunk.js"
+  },
+  {
+    "revision": "1d8dac689adb383324ae",
+    "url": "/would-you-rather/static/js/runtime-main.dd9dcef0.js"
+  },
+  {
+    "revision": "cb668a5a2d7bd76982a15412800f1c57",
+    "url": "/would-you-rather/static/media/reward.cb668a5a.png"
+  }
+]);
